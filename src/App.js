b4fe8b9474f06e44aadd592741e3160e8593efe1
Patrick Kuang <pkuang@ucsb.edu>
2020-04-02 01:15:05 -0700

    pk- added background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/nav";
+import Background from "./components/background"
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Background />    
     </div>
   );
 }
