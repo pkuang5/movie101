3f8da2cd8c47799161b7a0d52c5c9ae3eb6fdbcf
Patrick Kuang <pkuang@ucsb.edu>
2020-04-02 17:19:32 -0700

    pk- installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/nav";
-import Background from "./components/background"
+import "./styles/app.css";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Background />    
-    </div>
-  );
+    <p className = "text-red-300">Hello</p>
+  ); 
 }
 
 export default App;
