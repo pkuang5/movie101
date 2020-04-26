57f203e07d627cbf027b35f51a9a31e5b972a3c3
Patrick Kuang <pkuang@ucsb.edu>
2020-04-26 15:22:28 -0700

    pk- made navbar into own function, solves issue where you could be in two pages at once

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import "./styles/app.css";
-import { BrowserRouter as Router, Route, Redirect, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Login from "./components/login"
 import Feed from "./components/feed"
 import Editor from "./components/editor"
 import Films from "./components/films"
 import Profile from "./components/profilePage";
+import Navbar from "./components/navbar"
 
 class App extends Component {
   state = {
@@ -48,26 +49,13 @@ class App extends Component {
     else {
       return (
         <Router>
-          <nav class="flex items-center flex-wrap p-4 px-10 font-serif">
-            <div class="w-full block flex-grow flex items-end">
-              <div class="text-base flex-grow">
-                <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" />
-                  <circle cx="20.5" cy="17.5" r="10.5" stroke="black" stroke-width="2" />
-                  <line x1="28.1584" y1="25.244" x2="37.256" y2="34.3417" stroke="black" stroke-width="2" stroke-linecap="round" /></svg>
-              </div>
-              <div>
-                <div class="font-montserrat block inline-block mt-0 mr-4"><NavLink to="/" style={this.navLinkStyle} activeStyle={this.activeStyle}>Feed</NavLink></div>
-                <div class="font-montserrat block inline-block mt-0 mr-4"><NavLink to="/editor" style={this.navLinkStyle} activeStyle={this.activeStyle}>Editor</NavLink></div>
-                <div class="font-montserrat block inline-block mt-0 mr-4"><NavLink to="/films" style={this.navLinkStyle} activeStyle={this.activeStyle}>Films</NavLink></div>
-                <div class="font-montserrat block inline-block mt-0 mr-4"><NavLink to="/profile" style={this.navLinkStyle} activeStyle={this.activeStyle}>Profile</NavLink></div>
-                <div class="font-montserrat block inline-block mt-0 cursor-pointer mr-4" onClick={() => this.signInState(false, '')}>Logout</div>
-              </div>
-            </div>
-          </nav>
-          <Route path="/" exact strict component={() => <Feed signInState={this.signInState} googleId={this.state.googleId} />}></Route>
-          <Route path="/editor" exact strict component={Editor}></Route>
-          <Route path="/films" exact strict component={Films}></Route>
-          <Route path="/profile" exact strict component={Profile}></Route>
+          <Navbar signInState={this.signInState} />
+          <Switch>
+            <Route path="/feed" exact strict component={() => <Feed signInState={this.signInState} googleId={this.state.googleId} />}></Route>
+            <Route path="/editor" exact strict component={Editor}></Route>
+            <Route path="/films" exact strict component={Films}></Route>
+            <Route path="/profile" exact strict component={Profile}></Route>
+          </Switch>
         </Router>
       );
     }
