072909352107a9a8321516a30ad783835815bfb9
Patrick Kuang <pkuang@ucsb.edu>
2020-04-30 23:11:44 -0700

    pk - clean up google login

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -4,9 +4,9 @@ import firebase from '../firebaseConfig'
 
 class Profile extends Component {
     state = { 
-        username: 'pkuang',
-        profilePicUrl: 'https://scontent-sjc3-1.cdninstagram.com/v/t51.2885-19/s320x320/73714020_434465267246297_8758241746011291648_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_ohc=kMkRJd7b9ckAX_xG870&oh=691d19c0d078570edc3dd94409b2f03b&oe=5ECF02E0',
-        bio: "on a quest to find perpetual energy"
+        username: '',
+        profilePicUrl: '',
+        bio: ''
     }
 
     componentDidMount = () => {
