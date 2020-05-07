b25d3972bcfda5644342009b36eaefb2a0497d18
Nathaniel Lee <nateshot@Nathaniels-MacBook-Pro.local>
2020-05-07 00:42:35 -0700

    nl- editor backend done (except for instance where movie DNE)

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import firebase from '../firebaseConfig'
 import { render } from '@testing-library/react';
+import Noty from 'noty'
+import 'noty/lib/noty.css'
+import 'noty/lib/themes/bootstrap-v4.css'
 require('dotenv').config()
 
 class Editor extends Component {
@@ -8,28 +11,62 @@ class Editor extends Component {
         super(props);
         this.state = {
            movieName: '',
-           movieRating: '',
+           movieRating: '5',
            movieReview: '',
            movieYear: '',
            movieImage: '',
-           change: ''
+           change: '',
+           images: []
         };
-    }
+   }
+  showNotification = () => {
+      new Noty({
+          type: 'success',
+          theme: 'bootstrap-v4',
+          layout: 'bottomRight',
+          text: 'You Changes Have Been Saved!',
+          timeout: 3000
+      }).show()
+  }
+    showImage = () => {
+      new Noty({
+          type: 'success',
+          theme: 'bootstrap-v4',
+          layout: 'bottomRight',
+          text: 'Image has been selected!',
+          timeout: 3000
+      }).show()
+    } 
     getMovieInfo = () => {
+        this.state.images.length = 0; // deleted array upon every call
         let url = ''.concat('https://api.themoviedb.org/3/', 'search/movie?api_key=', process.env.REACT_APP_MOVIEDB_API_KEY, '&query=', this.state.movieName);
         fetch(url).then(result=>result.json()).then((data)=>{
-                this.setState({movieImage:'https://image.tmdb.org/t/p/w500'+data.results[0].poster_path}) 
+          let stateList = this.state.images;
+                var i
+                for (i in data.results) {
+            
+                  let movieEntry = {
+                    image: ('https://image.tmdb.org/t/p/w500'+data.results[i].poster_path),
+                    title: data.results[i]
+                  }
+                  if (movieEntry.image !== 'https://image.tmdb.org/t/p/w500null'){
+                    stateList.push(movieEntry)
+                  }
+                  
+                }
+                this.setState({images: stateList}) 
         })
     }
     sendSampleMovieEntry = () => {
         if (this.state.change) {
+          this.showNotification()
             let movieID = Math.floor(Math.random() * 10000)
             firebase.database().ref(`users/${this.props.googleId}/journals/${movieID}`).set({
                 name: this.state.movieName,
                 coverImage:  this.state.movieImage,
                 dateOfEntry: this.state.movieYear,
                 rating: this.state.movieRating,
-                description: 'good'
+                description: this.state.movieReview
             })  
         }
     }
@@ -56,19 +93,27 @@ class Editor extends Component {
             movieYear: e.target.value,
             change: true
         })
-        this.getMovieInfo()
     }
+    handleSearch = () => {
+      this.getMovieInfo()
+    }
+    handleImgClick = (e) => {
+      this.setState({
+        movieImage:e
+      })
+      this.showImage()
+    }
+    
     render() {
         return (
-          
             <form class="w-full max-w-lg">
+              <p>Fill in the following information, Search, and Submit!</p>
             <div class="flex flex-wrap -mx-3 mb-6">
               <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class=" text-gray-700 text-xs font-bold mb-2" >
                   Movie Name
                 </label>
                 <input onChange = {this.handleChangeMovieName} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Screenbook Documentary"/>
-                
               </div>
               <div class="w-full md:w-1/2 px-3">
                 <label class=" text-gray-700 text-xs font-bold mb-2" >
@@ -107,15 +152,25 @@ class Editor extends Component {
               </div>
               <div class="md:flex md:items-center">
                 <div class="md:w-2/3">
+                    <button onClick = {this.handleSearch}class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                        Search
+                     </button>
+                 </div>          
+                </div>
+              <div class="md:flex md:items-center">
+                <div class="md:w-2/3">
                     <button onClick = {this.sendSampleMovieEntry}class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
                         Submit
                      </button>
-                 </div>
-                </div>
+                 </div>          
+                </div>            
+                {this.state.images.map(movieEntry =>    
+                  <div class="flex flex-col w-32 h-auto items-end justify-start">
+                           <img  src={movieEntry.image} alt= {movieEntry.title} onClick = {() => this.handleImgClick(movieEntry.image)}/>
+                  </div>)}
             </div>
           </form>
         );
         }
 }
-
 export default Editor;
