a338016f2478beb6c531ae0223c88e36734fb621
Patrick Kuang <pkuang@ucsb.edu>
2020-05-08 16:14:29 -0700

    pk - added styling for selected tab for feeatured or journals

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -36,9 +36,9 @@ class Profile extends Component {
                     <p class="text-xl font-montserrat mt-2">{this.state.username}</p>
                     <p class="text-xs font-montserrat mt-1">{this.state.bio}</p>
                 </div>
-                <div class="flex w-4/5 justify-between mt-12">
-                    <button class="w-1/3 text-lg font-montserrat" onClick={() => this.handleFeatured(true)}>Featured</button>
-                    <button class="w-1/3 text-lg font-montserrat" onClick={() => this.handleFeatured(false)}>Journals</button>
+                <div class="flex w-4/5 text-lg font-montserrat justify-around select-none mt-12">
+                    <div class={this.state.featured ? 'border-b border-black px-1' : null} onClick={() => this.handleFeatured(true)}>Featured</div>
+                    <div class={!this.state.featured ? 'border-b border-black px-1' : null} onClick={() => this.handleFeatured(false)}>Journals</div>
                 </div>
                 <div class="mt-12 w-4/5 px-8">
                     <Gallery googleId={this.state.id} username={this.state.username} featured={this.state.featured}/>
