204ca46341c58ae5037c1a25d7957e5c571bea8d
Patrick Kuang <pkuang@ucsb.edu>
2020-05-09 02:45:36 -0700

    pk - implement search for movie in profile

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router-dom'
 function Gallery(props) {
 
     const [movies, setMovies] = useState([])
+    const [displayedMovies, setDisplayedMovies] = useState([])
     let history = useHistory();
 
     useEffect(() => {
@@ -18,7 +19,7 @@ function Gallery(props) {
         setMovies(movies.length=0)
         var userInfo = firebase.database().ref('users/' + props.googleId + '/journals')
         if (props.featured) userInfo = userInfo.orderByChild('featured').equalTo(true)
-        userInfo.on('value', (snapshot) => {
+        userInfo.once('value', (snapshot) => {
             let temp = [];
             snapshot.forEach((data) => {
                 let movie = {
@@ -29,6 +30,7 @@ function Gallery(props) {
                 temp.push(movie);
             });
             setMovies(movies.concat(temp));
+            setDisplayedMovies(movies.concat(temp));
         })
     }
 
@@ -36,14 +38,28 @@ function Gallery(props) {
         history.push("/" + props.username + "/movies/" + id);
     }
 
+    const handleSearch = (e) => {
+        let tempArr = movies;
+        setDisplayedMovies(tempArr.filter(function(value){ return value.name.toLowerCase().includes(e.target.value.toLowerCase());}))
+    }
+
     return (
-        <div class="grid grid-cols-4 col-gap-20 row-gap-10 grid-rows-2">
-            {movies.map(movieEntry =>
-                <div class="flex flex-col cursor-pointer justify-center" onClick={() => handleMovieClick(movieEntry.id)}>
-                    <img class="w-full" src={movieEntry.coverImageURL} alt={movieEntry.name} />
-                </div>
-            )}
-        </div>
+        <React.Fragment>
+            <div class="flex w-full mb-3">
+                <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" />
+                  <circle cx="20.5" cy="17.5" r="10.5" stroke="gray" stroke-width="2" />
+                  <line x1="28.1584" y1="25.244" x2="37.256" y2="34.3417" stroke="gray" stroke-width="2" stroke-linecap="round" />
+                </svg>
+                <input onChange={handleSearch} class="appearance-none w-full text-gray-700 border-black-600 mb-3 focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="search"/>
+            </div>
+            <div class="grid grid-cols-4 col-gap-20 row-gap-10 grid-rows-2">
+                {displayedMovies.map(movieEntry =>
+                    <div class="flex flex-col cursor-pointer justify-center" onClick={() => handleMovieClick(movieEntry.id)}>
+                        <img class="w-full" src={movieEntry.coverImageURL} alt={movieEntry.name} />
+                    </div>
+                )}
+            </div>
+        </React.Fragment>
     );
 
 }
