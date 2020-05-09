2011793f9464c2c0c773b6d1a2504b9fd6a7d582
Patrick Kuang <pkuang@ucsb.edu>
2020-05-09 16:45:04 -0700

    pk- code clean up for html in feed, movieRow, and profile

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -9,29 +9,25 @@ class Feed extends Component {
     
     componentDidMount = () => {
         var userInfo = firebase.database().ref('users/' + this.props.googleId)
-        userInfo.on('value', (snapshot) => {
+        userInfo.once('value', (snapshot) => {
             if (snapshot.val()) this.setState({firstName: snapshot.val().firstName})
         })
     }
 
     render() {
         return (
-            <React.Fragment>
-                <div class="flex justify-center pt-3 w-full">
-                    <div class="text-center w-2/3">
-                        <p class="font-serif text-3xl font-bold">Screenbook</p>
-                        <p class="font-montserrat"> Welcome, {this.state.firstName}! This is your feed. <br></br> See what movies are popular in the Screenbook community and follow other members to see their journals. Or, get started on your own journal. </p>
+                <div class="flex flex-col w-screen items-center">
+                    <div class="flex flex-col w-2/3 items-center">
+                        <div class="flex justify-center pt-3 w-full">
+                            <div class="text-center">
+                                <p class="font-serif text-3xl font-bold">Screenbook</p>
+                                <p class="font-montserrat"> Welcome, {this.state.firstName}! This is your feed. <br></br> See what movies are popular in the Screenbook community and follow other members to see their journals. Or, get started on your own journal. </p>
+                            </div>
+                        </div>
+                        <button class="button-color-beige hover:opacity-75 font-montserrat font-semibold text-white py-2 px-4 rounded w-40">+ add entry</button>
+                        <div class="w-full mt-3"><MovieRow/></div>
                     </div>
                 </div>
-                <div class="flex justify-center pt-4">
-                    <button class="button-color-beige hover:opacity-75 font-montserrat font-semibold text-white py-2 px-4 rounded w-40">+ add entry</button>
-                </div>
-                <div class="flex w-full justify-center mt-6"> 
-                    <div class="w-2/3">
-                        <MovieRow />
-                    </div>
-                </div>
-            </React.Fragment>
         );
     }
 }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -31,17 +31,19 @@ class Profile extends Component {
     render() {
         return (
             <div class="flex flex-col w-screen items-center">
-                <div class="flex flex-col w-1/3 items-center pt-6">
-                    <div class="rounded-full h-32 w-32 flex bg-cover" style={{backgroundImage: "url('" + this.state.profilePicUrl + "')"}}> </div>
-                    <p class="text-xl font-montserrat mt-2">{this.state.username}</p>
-                    <p class="text-xs font-montserrat mt-1">{this.state.bio}</p>
-                </div>
-                <div class="flex w-4/5 text-lg font-montserrat justify-around select-none mt-3">
-                    <div class={this.state.featured ? 'border-b border-black' : null} onClick={() => this.handleFeatured(true)}>Featured</div>
-                    <div class={!this.state.featured ? 'border-b border-black' : null} onClick={() => this.handleFeatured(false)}>Journals</div>
-                </div>
-                <div class="mt-3 w-4/5 px-8">
-                    <Gallery googleId={this.state.id} username={this.state.username} featured={this.state.featured}/>
+                <div class="w-2/3 items-center">
+                    <div class="flex flex-col items-center pt-6">
+                        <div class="rounded-full h-32 w-32 flex bg-cover" style={{backgroundImage: "url('" + this.state.profilePicUrl + "')"}}> </div>
+                        <p class="text-xl font-montserrat mt-2">{this.state.username}</p>
+                        <p class="text-xs font-montserrat mt-1">{this.state.bio}</p>
+                    </div>
+                    <div class="flex text-lg font-montserrat justify-around select-none mt-3">
+                        <div class={this.state.featured ? 'border-b border-black' : null} onClick={() => this.handleFeatured(true)}>Featured</div>
+                        <div class={!this.state.featured ? 'border-b border-black' : null} onClick={() => this.handleFeatured(false)}>Journals</div>
+                    </div>
+                    <div class="mt-3 px-8">
+                        <Gallery googleId={this.state.id} username={this.state.username} featured={this.state.featured}/>
+                    </div>
                 </div>
             </div>
         );
