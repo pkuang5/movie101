34d51903cebccae8ad457f024d4b112eb69db358
Patrick Kuang <pkuang@ucsb.edu>
2020-05-09 15:01:06 -0700

    pk- UI touchups

--- src/components/discover.jsx
+++ src/components/discover.jsx
@@ -25,17 +25,19 @@ function Discover(props) {
     }
 
     return (
-        <div class="flex flex-col mt-8 ml-8 font-montserrat">
-            <p class="text-xl font-semibold">Discover fellow users!</p>
-            {users.map(user => 
-                <div class="flex w-full border h-32 mb-3 pl-3 cursor-pointer" onClick={() => handleUserClick(user.username)}>
-                    <div class="self-center rounded-full h-24 w-24 flex bg-cover" style={{backgroundImage: "url('" + user.profilePicURL + "')"}}> </div>
-                    <div class="flex flex-col pl-4 pt-3">
-                        <p class="text-md">{user.username}</p>
-                        <p class="text-xs">{user.bio}</p>
+        <div class="flex flex-col mt-8 mx-8 font-montserrat w-screen items-center">
+            <div class="w-2/3">
+                <p class="text-2xl font-semibold">Discover fellow users!</p>
+                {users.map(user => 
+                    <div class="flex w-full h-32 mb-3 px-3 cursor-pointer hover:bg-gray-100" onClick={() => handleUserClick(user.username)}>
+                        <div class="self-center rounded-full h-24 w-24 flex bg-cover" style={{backgroundImage: "url('" + user.profilePicURL + "')"}}> </div>
+                        <div class="flex flex-col pl-4 pt-4">
+                            <p class="text-md font-semibold">{user.username}</p>
+                            <p class="text-xs">{user.bio}</p>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 }
