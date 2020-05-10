4ce306be08aa2490c566c66d7d4ea75cb0a70eb5
Patrick Kuang <pkuang@ucsb.edu>
2020-05-09 17:16:30 -0700

    pk - UI clean up for navbar and drop down dissapears after you are done with it

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -16,15 +16,13 @@ class Feed extends Component {
 
     render() {
         return (
-                <div class="flex flex-col w-screen items-center">
+                <div class="flex flex-col w-screen items-center mt-3">
                     <div class="flex flex-col w-2/3 items-center">
-                        <div class="flex justify-center pt-3 w-full">
-                            <div class="text-center">
-                                <p class="font-serif text-3xl font-bold">Screenbook</p>
-                                <p class="font-montserrat"> Welcome, {this.state.firstName}! This is your feed. <br></br> See what movies are popular in the Screenbook community and follow other members to see their journals. Or, get started on your own journal. </p>
-                            </div>
+                        <div class="flex flex-col items-center w-full">
+                            <p class="font-serif text-3xl font-bold">Screenbook</p>
+                            <p class="font-montserrat text-center"> Welcome, {this.state.firstName}! This is your feed. <br></br> See what movies are popular in the Screenbook community and follow other members to see their journals. Or, get started on your own journal. </p>
+                            <button class="button-color-beige hover:opacity-75 font-montserrat font-semibold text-white py-2 px-4 rounded w-40">+ add entry</button>
                         </div>
-                        <button class="button-color-beige hover:opacity-75 font-montserrat font-semibold text-white py-2 px-4 rounded w-40">+ add entry</button>
                         <div class="w-full mt-3"><MovieRow/></div>
                     </div>
                 </div>
