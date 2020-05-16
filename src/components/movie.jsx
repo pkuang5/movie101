9f4c543e6d8fc0f5c3deb7d6c4bfedbcc3227163
Patrick Kuang <pkuang@ucsb.edu>
2020-05-16 02:57:25 -0700

    pk - starting UI for movie

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -40,15 +40,31 @@ function Movie(props){
     }
 
     return (
-        <div class="w-screen text-4xl">
-            <p>id: {props.movieId}</p>
-            <p>movie name: {name}</p>
-            <p>date of entry: {dateOfEntry}</p>
-            <p>rating: {rating}</p>
-            <p>description: {description}</p>
-            <p>cover image url: {coverImage}</p>
-            <button class="bg-red-500 text-white px-3" onClick={handleDeleteMovie}>Delete movie</button>
-            <button class="bg-blue-500 text-white px-3" onClick={handleEditMovie}>Edit movie</button>
+        // <div class="w-screen text-4xl">
+        //     <p>id: {props.movieId}</p>
+        //     <p>movie name: {name}</p>
+        //     <p>date of entry: {dateOfEntry}</p>
+        //     <p>rating: {rating}</p>
+        //     <p>description: {description}</p>
+        //     <p>cover image url: {coverImage}</p>
+        //     <button class="bg-red-500 text-white px-3" onClick={handleDeleteMovie}>Delete movie</button>
+        //     <button class="bg-blue-500 text-white px-3" onClick={handleEditMovie}>Edit movie</button>
+        // </div>
+        <div class="flex w-screen justify-center mt-3">
+            <div class = "w-2/3">
+                <div class="flex justify-between font-montserrat">
+                    <p class="text-3xl font-semibold">{name}</p>
+                    <p class="text-xl">{dateOfEntry}</p>
+                </div>
+                <div class="flex">
+                    <div class = "w-1/4 h-24 bg-gray-200">
+                        <img src={coverImage}></img>
+                    </div>
+                    <div class = "w-3/4 h-24 bg-red-200">
+                        <p class="font-montserrat">{description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
