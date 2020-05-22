87c3e93b61e00b4e9256f0793338ad8d01d9ba6b
Nathaniel Lee <nateshot@Nathaniels-MacBook-Pro.local>
2020-05-22 14:30:06 -0700

    nl - delete layout template

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Layout from './components/layout'
 
 ReactDOM.render(
   <React.StrictMode>
