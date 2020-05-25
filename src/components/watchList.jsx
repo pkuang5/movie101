585828b7798634d7428ff3ba1566958cbcb5d2dc
Patrick Kuang <pkuang@ucsb.edu>
2020-05-24 22:40:24 -0700

    pk - edit watchlist functionality

--- src/components/watchList.jsx
+++ src/components/watchList.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useLocation, useHistory } from 'react-router'
 import firebase from '../firebaseConfig'
-
+import Noty from 'noty'
 
 function WatchList(props){
 
@@ -9,23 +9,27 @@ function WatchList(props){
     let history = useHistory()
 
     const [movies, setMovies] = useState([])
+    const [trashMovies, setTrashMovies] = useState([])
+    const [editMode, setEditMode] = useState(false)
 
     useEffect(() => {
-        firebase.database().ref('users/' + props.id + '/watchlist').once('value', (snapshot) => {
-            let temp = []
-            snapshot.forEach((data) => {
-                let movie = {
-                    id: data.key,
-                    image: data.val().image,
-                    title: data.val().title
-                }
-                temp.push(movie)
+        if (editMode === false) {
+            firebase.database().ref('users/' + props.id + '/watchlist').once('value', (snapshot) => {
+                let temp = []
+                snapshot.forEach((data) => {
+                    let movie = {
+                        id: data.key,
+                        image: data.val().image,
+                        title: data.val().title
+                    }
+                    temp.push(movie)
+                })
+                setMovies(movies.concat(temp))
             })
-            setMovies(movies.concat(temp))
-        })
+        }
         
         if (location.movieId) addMovie(location.movieId, location.image, location.title)
-    }, [location])
+    }, [location, editMode])
 
     function addMovie(movieId, posterImage, movieTitle) {
         firebase.database().ref(`users/${props.id}/watchlist/${movieId}`).set({
@@ -34,18 +38,45 @@ function WatchList(props){
         })
     }
 
+    function deleteMovies() {
+        trashMovies.map( movie => {
+            firebase.database().ref(`users/${props.id}/watchlist/${movie.id}`).remove()
+        })
+        setTrashMovies([])
+        setMovies([])
+        showNotification()
+    }
+
+    function showNotification(){
+        new Noty({
+            type: 'success',
+            theme: 'bootstrap-v4',
+            layout: 'bottomRight',
+            text: 'Your Changes Have Been Saved!',
+            timeout: 1000
+        }).show()
+    }
+
     return(
         <div class="flex flex-col w-screen items-center mt-3 px-6">
             <div class="lg:w-2/3 md:w-4/5 w-full items-center">
-                <p class="font-montserrat text-2xl">Watchlist</p>
+                <div class="flex justify-between w-full items-center">
+                    <p class="font-montserrat text-2xl">Watchlist</p>
+                    <div class="w-auto">
+                        <i class={!editMode ? "fa fa-edit fa-lg hover:text-gray-600 cursor-pointer": 'hidden'} onClick={() => setEditMode(true)}/>
+                        <i class={editMode ? "fa fa-times fa-lg hover:text-gray-600 cursor-pointer mr-3": 'hidden'} onClick={() => {setEditMode(false); setMovies([])}}/>
+                        <i class={editMode ? "fa fa-trash fa-lg hover:text-gray-600 cursor-pointer": 'hidden'} onClick={() => {setEditMode(false); deleteMovies()}}/>
+                    </div>
+                </div>
+                <p class="font-montserrat text-md">Keep track of the movies you want to watch in the future!</p>
                 <div class="grid grid-cols-3 sm:grid-cols-5 gap-4">
                     {movies.map( movie =>
-                        <img onClick={() => history.push(`/films/${movie.id}`)} class="transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110 cursor-pointer" src={movie.image} alt={movie.title} />
+                        <img onClick={() => editMode ? !trashMovies.includes(movie) ? setTrashMovies([...trashMovies,movie]): null : history.push(`/films/${movie.id}`)} class={trashMovies.includes(movie) ? "transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110 cursor-pointer border-yellow-400 border-solid border-4" : "transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110 cursor-pointer"} src={movie.image} alt={movie.title} />
                     )}
-                    <div onClick={() => history.push('/search')} class="flex flex-col w-full h-full bg-gray-200 items-center justify-center transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110 cursor-pointer">
+                    {editMode ? null : <div onClick={() => history.push('/search')} class="flex flex-col w-full h-full bg-gray-200 items-center justify-center transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110 cursor-pointer">
                         <i class="fa fa-plus-circle fa-2x"></i>
                         <p class="font-montserrat text-sm mt-1">Add movie</p>
-                    </div>
+                    </div>}
                 </div>
             </div>
         </div>
