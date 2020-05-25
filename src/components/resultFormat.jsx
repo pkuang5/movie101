ff8d8b7956ff08b97b23c5baf19507d1dc399c81
Patrick Kuang <pkuang@ucsb.edu>
2020-05-24 22:05:29 -0700

    pk - loaded movies into watchlist. can only add from search. must implement for film and movie component

--- src/components/resultFormat.jsx
+++ src/components/resultFormat.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom'
+import firebase from '../firebaseConfig'
 
 function ResultFormat(props) {
 
     let history = useHistory();
 
+    const [hover, setHover] = useState(false)
+    const [info, setInfo] = useState(false)
+    const [watchlist, setWatchlist] = useState(false)
+    const [journal, setJournals] = useState(false)
+
     if (props.searchType === 'Films') {
         return (
-            <div onClick={() => history.push('/films/' + props.result.id)} class="flex w-full h-24 font-montserrat my-2 cursor-pointer">
-                <img class="flex h-full" src={'https://image.tmdb.org/t/p/w500' + props.result.poster_path} alt='poster'></img>
-                <div class="ml-3 mt-3 flex-col">
-                    <div class="font-montserrat text-md font-semibold">{props.result.title}</div>
-                    <div class="text-xs mt-1">({props.result.release_date})</div>
+            <div o class="flex w-full h-24 font-montserrat my-2 cursor-pointer hover:bg-gray-100 pr-3" onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                <img class="flex h-full" src={'https://image.tmdb.org/t/p/w500' + props.result.poster_path} alt='poster' onClick={() => history.push('/films/' + props.result.id)}></img>
+                <div class="flex w-full justify-between items-center">
+                    <div class="ml-3 mt-3 flex-col" onClick={() => history.push('/films/' + props.result.id)}>
+                        <div class="font-montserrat text-md font-semibold">{props.result.title}</div>
+                        <div class="text-xs mt-1">({props.result.release_date})</div>
+                    </div>
+                    <div class={hover ? null:'sm:hidden'}>
+                        <div>
+                            <i class="fa fa-lg fa-info-circle mr-2 transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110" onMouseOver={() => setInfo(true)} onMouseLeave={() => setInfo(false)} onClick={() => history.push('/films/' + props.result.id)}/>
+                            <i class="fa fa-lg fa-eye mr-2 transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110" onMouseOver={() => setWatchlist(true)} onMouseLeave={() => setWatchlist(false)}  onClick={() => history.push({pathname:'/watchlist', movieId: props.result.id, image: 'https://image.tmdb.org/t/p/w500'+props.result.poster_path, title: props.result.title})}/>
+                            <i class="fa fa-lg fa-plus-circle mr-2 transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110" onMouseOver={() => setJournals(true)} onMouseLeave={() => setJournals(false)} onClick={() => history.push({pathname: '/editor', movieId: props.result.id, title:props.result.title})}/>
+                        </div>
+                        <div class='absolute font-montserrat text-xs'>
+                            <p class={info ? '': 'hidden'}>Movie Info</p>
+                            <p class={watchlist ? '': 'hidden'}>Add to watchlist</p>
+                            <p class={journal ? '': 'hidden'}>Add to journals</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- src/components/watchList.jsx
+++ src/components/watchList.jsx
@@ -1,8 +1,54 @@
 import React, {useState, useEffect} from 'react';
+import { useLocation, useHistory } from 'react-router'
+import firebase from '../firebaseConfig'
+
 
 function WatchList(props){
+
+    let location = useLocation()
+    let history = useHistory()
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        firebase.database().ref('users/' + props.id + '/watchlist').once('value', (snapshot) => {
+            let temp = []
+            snapshot.forEach((data) => {
+                let movie = {
+                    id: data.key,
+                    image: data.val().image,
+                    title: data.val().title
+                }
+                temp.push(movie)
+            })
+            setMovies(movies.concat(temp))
+        })
+        
+        if (location.movieId) addMovie(location.movieId, location.image, location.title)
+    }, [location])
+
+    function addMovie(movieId, posterImage, movieTitle) {
+        firebase.database().ref(`users/${props.id}/watchlist/${movieId}`).set({
+            image: posterImage,
+            title: movieTitle
+        })
+    }
+
     return(
-        <h1>Watch List</h1>
+        <div class="flex flex-col w-screen items-center mt-3 px-6">
+            <div class="lg:w-2/3 md:w-4/5 w-full items-center">
+                <p class="font-montserrat text-2xl">Watchlist</p>
+                <div class="grid grid-cols-3 sm:grid-cols-5 gap-4">
+                    {movies.map( movie =>
+                        <img onClick={() => history.push(`/films/${movie.id}`)} class="transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110 cursor-pointer" src={movie.image} alt={movie.title} />
+                    )}
+                    <div onClick={() => history.push('/search')} class="flex flex-col w-full h-full bg-gray-200 items-center justify-center transition ease-in-out duration-200 transform hover:-translate-y-1 hover:scale-110 cursor-pointer">
+                        <i class="fa fa-plus-circle fa-2x"></i>
+                        <p class="font-montserrat text-sm mt-1">Add movie</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
