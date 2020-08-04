97f3555834c77e3f83cdd8c03811e62f024bc558
Nathaniel Lee <nateshot@Nathaniels-MacBook-Pro.local>
2020-08-04 02:12:16 -0700

    nl - added all new comment system changes to mobile

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -442,17 +442,22 @@ function Movie(props){
                     </div>
                     <div class = "h-auto mt-4"  >
                         {showComments?commentsToDisplay.map(firstItem => 
-                            <div class = "mt-4 p-2 border-2 border-gray-400 h-auto flex flex-row ">
-                                <div class= "flex-grow-0 flex-shrink-0 flex-row rounded-full h-16 w-16 flex bg-cover justify-center mr-8 pt-8 cursor-pointer bg-white" onClick = {()=>history.push('/' + firstItem.username)} style={{backgroundImage: "url('" + firstItem.profilePic + "')"}}/>
-                                <div class = "flex flex-col">
-                                    <div class = "cursor-pointer" onClick = {()=>history.push('/' + firstItem.username)}>
-                                        {firstItem.username} 
-                                    </div>
-                                    <div class = "text-sm mt-2">
-                                        {firstItem.comment}
+                            <div class = "mt-4 p-2 border-2 border-gray-400 h-auto flex flex-row justify-between ">
+                                <div class = "flex flex-row">
+                                    <div class= "flex-grow-0 flex-shrink-0 flex-row rounded-full h-16 w-16 flex bg-cover justify-center mr-8 pt-8 cursor-pointer bg-white" onClick = {()=>history.push('/' + firstItem.username)} style={{backgroundImage: "url('" + firstItem.profilePic + "')"}}/>
+                                    <div class = "flex flex-col text-sm">
+                                        <div class = "cursor-pointer" onClick = {()=>history.push('/' + firstItem.username)}>
+                                            {firstItem.username} 
+                                        </div>
+                                        <div class = "mt-2">
+                                            {firstItem.comment}
+                                        </div>
+                                        
                                     </div>
                                 </div>
+                                    {props.username === presentUsername || presentUsername === firstItem.username ? <i class="fa fa-trash fa-lg hover:text-gray-600 cursor-pointer" onClick={()=>deleteComment(firstItem)}></i>:null}
                             </div>
+                            
                         ):null}
                     </div>
                 </div>
